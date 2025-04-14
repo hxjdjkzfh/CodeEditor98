3701a7a4473b279f38dd8683ce9907221712c804
Arthur <arthur@example.com>
2025-04-14 18:58:41 -0400

    fix: move .aar implementations into dependencies block

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,6 +46,8 @@ repositories {
 }
 
 dependencies {
+    implementation(files("libs/editor-release.aar"))
+    implementation(files("libs/language-textmate.aar"))
     implementation(files("app/libs/language-textmate.aar"))
     implementation(files("app/libs/editor-release.aar"))
     implementation(files("libs/editor-release.aar"))
@@ -64,6 +66,8 @@ dependencies {
 }
 
 dependencies {
+    implementation(files("libs/editor-release.aar"))
+    implementation(files("libs/language-textmate.aar"))
     implementation(files("app/libs/language-textmate.aar"))
     implementation(files("app/libs/editor-release.aar"))
     implementation(files("libs/editor-release.aar"))
