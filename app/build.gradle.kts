a473f5b835ef58c891039d4c42d2becf7a1a6432
Arthur <arthur@example.com>
2025-04-13 20:50:04 -0400

    fix: load SoraEditor v0.21.0 from JitPack

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,11 +43,11 @@ android {
 repositories {
     google()
     mavenCentral()
-    maven { url = uri("https://jitpack.io") }
+    maven(url = "https://jitpack.io")
 }
 
 dependencies {
-    implementation("com.github.Rosemoe.sora-editor:editor:0.21.1")
+    implementation("com.github.Rosemoe.sora-editor:editor:v0.21.0")
 
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
