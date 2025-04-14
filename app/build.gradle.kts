c0b9475bfd420c20f32990652fa21229b31945a7
Arthur <arthur@example.com>
2025-04-14 17:51:57 -0400

    fix: add local language-textmate AAR

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,6 +51,7 @@ dependencies {
     implementation(files("libs/editor-release.aar"))
     implementation(files("libs/language-textmate.aar"))
     implementation(files("libs/language-textmate.aar"))
+    implementation(files("libs/language-textmate.aar"))
     implementation("io.github.rosemoe.sora-editor:language-textmate:0.21.0")
 
     implementation("androidx.core:core-ktx:1.12.0")
@@ -66,6 +67,7 @@ dependencies {
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.compose.material3:material3:1.1.2")
     implementation(files("libs/editor-release.aar"))
+    implementation(files("libs/language-textmate.aar"))
     implementation(files("libs/language-textmate.aar"))
     implementation(files("libs/language-textmate.aar"))
     implementation("io.github.rosemoe.sora-editor:language-textmate:0.21.0")
