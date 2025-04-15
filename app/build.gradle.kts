1d7464dc9f91fa4af54308f820c391b2d7e113a9
Arthur <arthur@example.com>
2025-04-15 08:39:37 -0400

    Fix: use official JitPack release of SoraEditor

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,7 +49,7 @@ repositories {
 
 
 dependencies {
-    implementation("com.github.rosemoe:sora:6fd803de70f89b56e427f42dc7f4622c3529604c")
+    implementation("com.github.rosemoe.sora:editor:0.20.0")
     implementation(files("libs/editor-release.aar"))
     implementation(files("libs/language-textmate.aar"))
     implementation("com.google.android.material:material:1.11.0")
